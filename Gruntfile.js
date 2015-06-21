05e13895319ffdb8a6debbdad21a7aa5b44d4d21
Nacho Yegro <nachoyegro@gmail.com>
2015-06-21 18:56:25 -0300

    karma conf 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,20 @@ module.exports = function(grunt) {
 
     jshint: {
       all: ['Gruntfile.js', 'hello.js', 'app.js', 'models/*.js', 'routes/*.js']
+    },
+
+    karma: {
+      unit: {
+        options: {
+          files: ['test/**/*.js']
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
   grunt.registerTask('default', 'jshint');
 };
