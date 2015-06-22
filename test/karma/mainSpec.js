cbcdced2ecd87e61829858fbdef83b83ba58413a
Nacho Yegro <nachoyegro@gmail.com>
2015-06-22 00:24:08 -0300

    por fin controller con karma

--- test/karma/mainSpec.js
+++ test/karma/mainSpec.js
@@ -11,3 +11,23 @@ describe('Testing Vacaciones-Permanentes Module', function() {
     expect(mainModule).toBeDefined();
   });
 });
+
+describe('Main Ctrl', function () {
+
+  var tripMockeado = {'name': 'Bariloche'};
+
+  beforeEach(module('vacacionesPermanentes'));
+
+  describe('MainCtrl', function () {
+
+
+    it('should clean scope after add ', inject(function ($controller) {
+      var $scope = {'trip':tripMockeado};
+      $controller('MainCtrl', { $scope: $scope }); 
+      expect($scope.trip.name).toBe('Bariloche');
+      $scope.addTrip();
+      expect($scope.trip.name).toBe('');
+    }));
+
+  });
+});
--- test/karma/tripsCtrlSpec.js
+++ test/karma/tripsCtrlSpec.js
@@ -1,20 +1,20 @@
 'use strict';
-/
-describe('Testing Trips Controller', function() {
-  var _scope, TripsController;
 
-  beforeEach(function() {
-    module('vacacionesPermanentes');
+describe('Main Controller', function() {
+  var ctrl, scope, serviceTrips, serviceTrip;
 
-    inject(function($rootScope, $controller) {
-      _scope = $rootScope.$new();
-      TripsController = $controller('TripsCtrl', {
-        $scope: _scope
-      });
-    });
-  });
+  beforeEach(module('vacacionesPermanentes'));
+  beforeEach(inject(function($controller, $rootScope, trips, trip) {
 
-  it('Should be registered', function() {
-    expect(TripsController).toBeDefined();
-  });
-});
+  scope = $rootScope.$new();
+  serviceTrips = trips;
+  serviceTrip = {'destinations':[]};
+  //Create the controller with the new scope
+  ctrl = $controller('TripsCtrl', {$scope: scope, trips: serviceTrips, trip:serviceTrip});
+}));
+
+    it('should clean scope after add ', inject(function ($controller) {
+      expect(true).toBe(true);
+    }));
+
+})
